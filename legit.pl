5cda5788d3c789c2978605421ff0acee5227e43f
freon-lunarion <antonio.daniswara@gmail.com>
2018-09-28 18:33:35 +1000

    rm command

--- legit.pl
+++ legit.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/perl -w
 use File::Copy;
 use File::Find;
+use Digest::MD5 qw(md5_hex);
+use File::Slurp;
 
 use constant DIR => ".legit";
 use constant INDEX => ".legit/index/";
 use constant REPO => ".legit/repository/";
 use constant LOG => ".legit/log";
 use constant TEMP => ".legit/temp";
+
+sub _getLastCommit;
+sub _isSameFiles;
 
 sub add;
 sub branch;
@@ -38,11 +43,11 @@ if ($cmd eq "add") {
 } elsif ($cmd eq "merge") {
 
 } elsif ($cmd eq "rm") {
-
+    rm(@ls);
 } elsif ($cmd eq "show") {
     show($ARGV[1]);
 } elsif ($cmd eq "status") {
-
+    status();
 } else {
 
 }
@@ -74,6 +79,48 @@ sub add {
     }
 }
 
+sub rm {
+    @ls = @_;
+    my $isCurDir = 0;
+    my $isForce = 0;
+    my $lastCommit = _getLastCommit();
+
+    if ( grep $_ eq "--force", @ls ) {
+        $isForce = 1;
+    }
+
+    if (! grep $_ eq "--cache", @ls ) {
+        $isCurDir = 1;
+    }
+
+    $dirCommit = REPO.$lastCommit."/";
+    if ($isForce == 0 ){
+        foreach $file (@ls) {
+            if (! _isSameFiles(INDEX.$file, $dirCommit.$file) ){
+                die "index file diffrent from last commit";
+            }
+
+            if ($isCurDir == 1) {
+                if (! _isSameFiles(INDEX.$file, $dirCommit.$file) ){
+                    die "current file diffrent from last commit";
+                }
+            }
+        }
+
+    }
+
+    foreach $file (@ls) {
+        if ($file ne '--cache' &&  $file ne '--force') {
+            unlink INDEX.$file;
+            if ($isCurDir == 1) {
+                unlink $file; 
+            }
+        }
+    }
+
+
+}
+
 sub commit {
     if ( grep $_ eq "-a", @_ ) {
         opendir my $dir, INDEX;
@@ -89,29 +136,36 @@ sub commit {
         mkdir REPO;
     } 
 
-    $dirnum = -1;
+    $filenum = -1;
     find(
-    sub {
-        -d && $dirnum++;
-        # print $FILE::Find::name, "\n";
-    },
-    REPO);
-    # print $dirnum,"\n";
+        sub {
+            -e && $filenum++;
+        },INDEX
+    );
+
+    if ($filenum <= 0) {
+        die "no file to commit";
+    }
+
+    $dirnum = _getLastCommit();
+    if ($dirnum < 0) {
+        $dirnum = 0;
+    } else {
+        $dirnum += 1;
+    }
+    
     mkdir REPO.$dirnum;
 
     opendir my $dir, INDEX;
-        while (my $thing = readdir $dir) {
-            if ($thing eq '.' or $thing eq '..') {
-                next;
-            }
-            copy(INDEX.$thing,REPO.$dirnum."/".$thing);
+    while (my $thing = readdir $dir) {
+        if ($thing eq '.' or $thing eq '..') {
+            next;
         }
+        copy(INDEX.$thing,REPO.$dirnum."/".$thing);
+    }
     closedir $dir;
 
     $msg = pop @_;
-    
-    # print $msg,"\n";
-
     if (-e LOG ) {
         open my $new, ">", TEMP;
         open my $old, "<", LOG;
@@ -167,4 +221,28 @@ sub show {
     }
     close FL;
     print "\n";
+}
+
+sub _getLastCommit {
+    $dirnum = -2;
+    if (-d REPO) {
+        find(
+        sub {
+            -d && $dirnum++;
+            # print $FILE::Find::name, "\n";
+        },
+        REPO);
+    }
+    
+
+    return $dirnum;
+}
+
+sub _isSameFiles {
+    my ($file_1, $file_2) = @_;
+    if (md5_hex(read_file($file_1)) eq md5_hex(read_file($file_2))) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
